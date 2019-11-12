5a2ac65adaee77e65b1f21abee3abd891f552745
NkodoMbeJeanYves <nkodomjy@gmail.com>
2019-11-12 17:07:47 +0100

    button scroll design completed

--- js/buttonScroll.js
+++ js/buttonScroll.js
@@ -1,25 +1,40 @@
 //Get the button:
 mybutton = document.getElementById("myBtn");
 
+
 btnbottom = document.getElementById('btn_bottom');
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 788 || document.documentElement.scrollTop > 788) {
-    mybutton.style.display = "block";
-    btnbottom.style.display = "none";
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    $(mybutton).fadeIn('slow')
   } else {
-    mybutton.style.display = "none";
-    btnbottom.style.display = "block";  
+    $(mybutton).fadeOut('slow') 
   }
-
-
 }
 
+/*$(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+      //$('#header').addClass('header-fixed');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+      //$('#header').removeClass('header-fixed');
+    }
+  });*/
+
+/*$('.back-to-top').click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1500, 'easeInOutExpo');
+    return false;
+  });*/
 
 
 var intervalId = null;
+
+
 
  function scrolltop(){
   let position = window.scrollY - 10;
@@ -36,14 +51,11 @@ var intervalId = null;
 }
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  $('#myBtn').css('visibility','hidden');
+  //$('#myBtn').css('visibility','hidden');
   intervalId = setInterval(scrolltop, 1);
+
 }
 
-function bottomFunction() {
-  $('#myBtn').css('visibility','hidden');
-  intervalId = setInterval(checkdown, 1);
-}
 
 
 
